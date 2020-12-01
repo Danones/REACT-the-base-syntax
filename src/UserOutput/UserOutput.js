bb0fd62a50d88b7a474658e13ea9e11923c03584
Danones <diogo.sousa.uk@gmail.com>
2020-12-01 22:23:53 +0000

    two components added. State management

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserOutput = (props) =>{
+    return (
+        <div className="Useroutput">
+            <p>Username: {props.username}</p>
+            <p>Hello</p>
+        </div>
+    );
+}
+
+export default UserOutput;
